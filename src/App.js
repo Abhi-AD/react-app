612f259ae599d8796228b060855eecafe5b4dc80
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2023-12-31 17:32:51 +0545

    add the bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import Navbar from './compoents/Nabar';
+import Contact from './compoents/Contact';
+import Form from './compoents/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar/>
+      <Contact/>
+      <Form/>
     </div>
   );
 }
---
+++ src/compoents/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap'
+
+
+
+class Contact extends React.Component {
+     render() {
+          return (
+               <h1>Contact ME</h1>
+          )
+     }
+}
+export default Contact;
